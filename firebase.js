a68bde1f97b5c2a69f96a4d5bd304168c38dae9b
Zebedee Marsh <zjnmarsh@gmail.com>
2026-03-29 12:59:43 +0100

    keys

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getFirestore, collection, addDoc, onSnapshot, query, orderBy, Timestamp
 
 // Replace with your project's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyA4QgG7EbX-6nVuUXocVK0W5cZS5LJlxAY",
+  authDomain: "soapstone-8373a.firebaseapp.com",
+  projectId: "soapstone-8373a",
+  storageBucket: "soapstone-8373a.firebasestorage.app",
+  messagingSenderId: "138436646343",
+  appId: "1:138436646343:web:6d6a6b4ec2dbd73878f5e2",
+  measurementId: "G-C3RSPDYXD9"
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
